824a4ba170866d5e9b0a38961453603aba997e96
YoussefELALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2026-04-02 11:31:33 +0200

    feat: Create the request DTO with validation

--- src/main/java/com/observability/platform/api/dto/request/EventRequest.java
+++ src/main/java/com/observability/platform/api/dto/request/EventRequest.java
@@ -1,4 +1,35 @@
 package com.observability.platform.api.dto.request;
 
+import com.observability.platform.domain.model.LogLevel;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EventRequest {
+
+    @NotBlank
+    @Size(max = 100)
+    @Pattern(regexp = "^[a-zA-Z0-9/_-]+$", message = "service must be alphanumeric with - / _" )
+    private String service;
+
+    @NotNull
+    @PastOrPresent
+    private Instant timestamp;
+
+    @NotNull
+    private LogLevel level;
+
+    @Size(max = 5000)
+    private String message;
+
+    @Valid
+    @NotNull
+    private MetadataRequest metadata;
 }
--- src/main/java/com/observability/platform/api/dto/request/MetadataRequest.java
+++ src/main/java/com/observability/platform/api/dto/request/MetadataRequest.java
@@ -1,4 +1,19 @@
 package com.observability.platform.api.dto.request;
 
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+
 public class MetadataRequest {
+
+    @NotBlank
+    @Size(max = 100)
+    private String requestId;
+
+    @Positive
+    private String latencyMs;
+
+    @DecimalMin("0.0")
+    @DecimalMax("1.0")
+    private BigDecimal cpuUsage;
 }
